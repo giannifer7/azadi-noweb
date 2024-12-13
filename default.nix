7bd590c4a79da0d8af853f0344a99b7a357f3035
giannifer7 <gianni.ferrarotti@gmail.com>
2024-12-13 07:43:27 +0100

    changed default.nix and cargo

--- default.nix
+++ default.nix
@@ -11,6 +11,12 @@ pkgs.rustPlatform.buildRustPackage rec {
     lockFile = ./Cargo.lock;
   };
 
+  # Don't run tests in parallel
+  cargoParallelTestThreads = 1;
+
+  # Skip the cargo config to avoid metadata parsing issues
+  CARGO_CONFIG = "";
+
   meta = with pkgs.lib; {
     description = "A Rust implementation of noweb-style literate programming tool";
     homepage = "https://github.com/giannifer7/azadi-noweb";
