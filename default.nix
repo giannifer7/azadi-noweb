af73f6f131b2d6de6d60a2a82f94886b27a209e6
giannifer7 <gianni.ferrarotti@gmail.com>
2024-12-13 08:03:39 +0100

    default.nix

--- default.nix
+++ default.nix
@@ -1,21 +1,41 @@
 # default.nix
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {
+    config = {
+      allowUnfree = true;
+      permittedInsecurePackages = [];
+    };
+  }
+}:
 
-pkgs.rustPlatform.buildRustPackage rec {
+let
+  naersk = pkgs.callPackage (pkgs.fetchFromGitHub {
+    owner = "nix-community";
+    repo = "naersk";
+    rev = "aeb58d5e8faead8980a807c840232697982d47b9";
+    sha256 = "sha256:0l2me2j8i4zvm3hg5fd8j27qg4bjgvj3zm9nvxqz2q1qg2r8584g";
+  }) {};
+in
+
+naersk.buildPackage {
   pname = "azadi-noweb";
-  version = "0.1.2";  # This should match your Cargo.toml
+  version = "0.1.2";
 
   src = ./.;
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
+  buildInputs = with pkgs; [
+    pkg-config
+  ];
+
+  overrideMain = attrs: {
+    # Skip tests during the build
+    doCheck = false;
+
+    # Add any necessary build inputs
+    nativeBuildInputs = (attrs.nativeBuildInputs or []) ++ (with pkgs; [
+      rustc
+      cargo
+    ]);
   };
-
-  # Don't run tests in parallel
-  cargoParallelTestThreads = 1;
-
-  # Skip the cargo config to avoid metadata parsing issues
-  CARGO_CONFIG = "";
 
   meta = with pkgs.lib; {
     description = "A Rust implementation of noweb-style literate programming tool";
